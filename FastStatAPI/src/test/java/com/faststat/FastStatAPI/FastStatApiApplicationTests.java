2413dce20cddca5615e3c2ecceaa0bd73ba2c0df
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-09-21 21:29:46 -0500

    initial project setup

---
+++ FastStatAPI/src/test/java/com/faststat/FastStatAPI/FastStatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faststat.FastStatAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastStatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
